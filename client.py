0a0b14597d42b29f188d0e9a8fe11928d7a2d747
Aryan <Aryan@ARYANS-LAPTOP>
2017-10-12 17:26:13 -0500

    Can now compute the sum of a list of space-separated integers.

--- client.py
+++ client.py
@@ -1,12 +1,19 @@
 #! /usr/bin/python
 import socket
+
 host="127.0.0.1"
 port=12345
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+# Connect to port 12345 on localhost
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((host,port))
-print("ENTER NO TO BE SENT:")
-a=raw_input()
-s.sendall(a)
-data=s.recv(1024)
+
+# Take input from a user, encode it to utf-8, and send to server
+a = input("Enter space-separated integers: -\n")
+s.sendall(a.encode("utf-8"))
+
+data = s.recv(1024)
 s.close()
-print("RECIEVED DATA :",data)
+
+# Convert received data from bytes to integers
+print("Received data: ", int.from_bytes(data, byteorder='big'))
--- server.py
+++ server.py
@@ -1,18 +1,27 @@
 #! /usr/bin/python
 import socket
-host="127.0.0.1"
-port=12345
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind((host,port))
+from functools import reduce
+
+host = "127.0.0.1"
+port = 12345
+
+# Create a server a bind to port 12345 on localhost
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
 s.listen(1)
 
 while True:
-	conn,addr=s.accept()
-	print("Connected by:",addr)
-	data=conn.recv(1024)
-	if not data: break
-	a=[int(i) for i in data.split(' ')]
-	k=a[0]+a[1]
-	k=str(k)
-	conn.sendall(k)
-conn.close
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
+    data = conn.recv(1024)
+    if not data: break
+
+    # Convert input into a list of integers
+    num_list = list(map(lambda x: int(x), data.decode("utf-8").split(' ')))
+
+    # Use reduce to compute sum of list
+    num_sum = reduce((lambda x, y: x + y), num_list)
+
+    # Convert sum to bytes and send to client
+    conn.sendall(num_sum.to_bytes(2, byteorder='big'))
+conn.close()
