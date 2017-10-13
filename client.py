f89fc385f40daaaad58288ede20fe75e5a8955b9
blanyal <blanyal@hotmail.com>
2017-10-12 20:11:28 -0500

    Added command line parameters to enter host address and port number

--- client.py
+++ client.py
@@ -1,12 +1,35 @@
 #! /usr/bin/python
 import socket
-host="127.0.0.1"
-port=12345
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((host,port))
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# Command line argument for Server Address
+# If left blank defaults to localhost
+parser.add_argument("Host",
+                    help="Enter the host",
+                    type=str,
+                    nargs="?",
+                    const=1,
+                    default="127.0.0.1")
+
+# Command line argument for Port Number
+# If left blank defaults to 8080
+parser.add_argument("Port",
+                    help="Enter the port",
+                    type=int,
+                    nargs="?",
+                    const=1,
+                    default=12345)
+
+host = parser.parse_args().Host
+port = parser.parse_args().Port
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
 print("ENTER NO TO BE SENT:")
-a=raw_input()
+a = raw_input()
 s.sendall(a)
-data=s.recv(1024)
+data = s.recv(1024)
 s.close()
-print("RECIEVED DATA :",data)
+print("RECEIVED DATA :", data)
